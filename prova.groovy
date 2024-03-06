1809bd123c433502fa3ee3339d0b0749cce1301a
richianguilletti <riccardo2002@icloud.com>
2024-03-06 16:44:43 +0100

    .

--- prova.groovy
+++ prova.groovy
@@ -70,9 +70,9 @@ pipeline {
                 script {
                     sh "oc project ${OPENSHIFT_PROJECT}"
 
-                    sh "oc new-app --name=${APPLICATION_NAME} ${DOCKER_IMAGE_NAME_TAG} --port=8080"
+                    //sh "oc new-app --name=${APPLICATION_NAME} ${DOCKER_IMAGE_NAME_TAG}"
 
-                    //sh "oc create deployment ${APPLICATION_NAME}  --image=${DOCKER_IMAGE_NAME_TAG}"
+                    sh "oc create deployment ${APPLICATION_NAME}  --image=${DOCKER_IMAGE_NAME_TAG} --port=8080"
 
                     //sh "oc apply -f path/to/tuo/file-di-configurazione.yaml"
 
