4668f3691bc1b715b41e44e7afd68e5ad3e6f2dc
richianguilletti <riccardo2002@icloud.com>
2024-03-06 14:51:52 +0100

    .

--- prova.groovy
+++ prova.groovy
@@ -8,7 +8,7 @@ pipeline {
         OPENSHIFT_NAME_APP = ""
         DOCKER_IMAGE_NAME_TAG = "docker.io/hello-world"
         DOCKER_URL = "registry.connect.redhat.com"
-        APPLICATION_NAME = "ciao_mondo"
+        APPLICATION_NAME = "ciaomondo"
         CREDENTIALS_ID = "OCP-API-KEY"
         TOKEN=""
     }
