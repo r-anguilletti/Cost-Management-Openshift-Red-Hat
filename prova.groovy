48884c559d860df32f0b10445937b66573a94227
richianguilletti <riccardo2002@icloud.com>
2024-03-13 08:47:39 +0100

    tag dei pod

--- prova.groovy
+++ prova.groovy
@@ -118,7 +118,7 @@ pipeline {
                     /*
 
                     sh "oc get pods > temp.txt"
-                    pod=sh (script: "cat temp.txt | egrep -e \"${NOME}\" | egrep -e \"Running\" | tr -s " " | cut -d " " -f 1", returnStdout: True).trim()
+                    def pod=sh (script: "cat temp.txt | egrep -e \"${NOME}\" | egrep -e \"Running\" | tr -s \" \" | cut -d \" \" -f 1", returnStdout: true)
                     sh "oc label pod/${pod} ambiente=${Ambiente}"
                     sh "oc label pod/${pod} ruolo=${Ruolo}"
                     sh "oc label pod/${pod} servizio=${Servizio}"
@@ -129,7 +129,7 @@ pipeline {
 
                     */
 
-                    //sh "rm temp.txt"
+                    
                 }
             }
         }
