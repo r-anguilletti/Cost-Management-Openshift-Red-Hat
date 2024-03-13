2ea9b77e22c706233d82bd76f2e80d781d4885c3
richianguilletti <riccardo2002@icloud.com>
2024-03-13 10:05:09 +0100

    clone repository

--- prova.groovy
+++ prova.groovy
@@ -18,7 +18,7 @@ pipeline {
     stages {
         stage('Clone Repository') {
             steps {
-                git 'https://github.com/r-anguilletti/Cost-Management-Openshift-Red-Hat'
+                git branch: 'main', url: "https://github.com/r-anguilletti/Cost-Management-Openshift-Red-Hat"
             }
         }
 
